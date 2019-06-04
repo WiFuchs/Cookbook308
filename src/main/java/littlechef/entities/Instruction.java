66b2b8657c1e2f820037797b3f952464fc4a129e
Will Fuchs <wfuchs@calpoly.edu>
2019-06-04 09:58:22 -0700

    change column size for instruction

--- src/main/java/littlechef/entities/Instruction.java
+++ src/main/java/littlechef/entities/Instruction.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -24,6 +25,7 @@ public class Instruction {
 	@GeneratedValue
 	private Long id;
 
+	@Column(length = 500)
 	private String step;
 
 	@ManyToOne(cascade = CascadeType.ALL)
@@ -35,7 +37,7 @@ public class Instruction {
 	@OneToMany(mappedBy = "stepID", orphanRemoval = true, cascade = CascadeType.ALL)
 	private List<InstructionAnnotation> annotations = new ArrayList<>();
 	
-
+	
 	public Instruction() {
 	}
 
